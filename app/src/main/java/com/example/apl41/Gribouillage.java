f50099a9b186fc6a1a8261aae101db6aa410546d
pain <anatole.pain@outook.fr>
2021-02-02 00:31:25 +0100

    ajout Gribouillage

---
+++ app/src/main/java/com/example/apl41/Gribouillage.java
@@ -0,0 +1,11 @@
+package com.example.apl41;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class Gribouillage extends View {
+    public Gribouillage(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
